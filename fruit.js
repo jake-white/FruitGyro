118edb47b5863e7e1973005671d5b68b818d6f7b
Jake White <squirrelboy1225@gmail.com>
2018-10-18 19:54:48 -0400

    Testing positioning code and acceleration

--- fruit.js
+++ fruit.js
@@ -1,17 +1,26 @@
 let game, basket, accelerometer;
+let width = window.innerWidth;
+let height = window.innerHeight;
+let basketAccel = 0;
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
     game = new Scene();
-    basket = new Sprite(game, "basket.png", 50, 50);
+    game.setSize(900, 1600);
+    basket = new Sprite(game, "basket.png", 300, 300);
+    basket.setPosition(game.width/2, game.height-basket.height);
     accelerometer = new Accel();
+    game.canvas.getContext("2d").imageSmoothingEnabled= false;
     game.start();
 });
 
 function update() {
     game.clear();
-    let dx = accelerometer.getAX();
-    dx*=5;
-    basket.setDX(dx);
+    basketAccel-= 5*accelerometer.getAX();
+    basket.setDX(basketAccel);
     basket.update();
+}
+
+function Fruit() {
+    
 }
